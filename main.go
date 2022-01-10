9f3778252b11dbc1bae96ca836350c5d145d9366
Kento Kawakami <kento.kawakami@linecorp.com>
2022-01-10 16:15:48 +0900

    Apply format

--- main.go
+++ main.go
@@ -1,29 +1,28 @@
 package main
 
 import (
-	"fmt"
-	"time"
-	"flag"
 	"crypto/tls"
-	"net/url"
 	"encoding/json"
+	"flag"
+	"fmt"
+	"net/url"
+	"time"
 
 	vegeta "github.com/tsenart/vegeta/v12/lib"
 )
 
-
-func AttackByRate(attacker vegeta.Attacker, targeter vegeta.Targeter, attackRate int, duration time.Duration) vegeta.Metrics{
-	 var metrics vegeta.Metrics
+func AttackByRate(attacker vegeta.Attacker, targeter vegeta.Targeter, attackRate int, duration time.Duration) vegeta.Metrics {
+	var metrics vegeta.Metrics
 	rate := vegeta.Rate{Freq: attackRate, Per: time.Second}
-	 for res := range attacker.Attack(targeter, rate, duration, "Big Ban") {
-		 metrics.Add(res)
-	 }
-	 metrics.Close()
+	for res := range attacker.Attack(targeter, rate, duration, "Big Ban") {
+		metrics.Add(res)
+	}
+	metrics.Close()
 
-	 return metrics
+	return metrics
 }
 
-func binarrySearch(attacker vegeta.Attacker, targeter vegeta.Targeter, maxRate int, duration time.Duration, delay time.Duration) vegeta.Metrics{
+func binarrySearch(attacker vegeta.Attacker, targeter vegeta.Targeter, maxRate int, duration time.Duration, delay time.Duration) vegeta.Metrics {
 
 	var metrics vegeta.Metrics
 	minRate := 0
@@ -34,13 +33,13 @@ func binarrySearch(attacker vegeta.Attacker, targeter vegeta.Targeter, maxRate i
 		}
 
 		metrics = AttackByRate(attacker, targeter, midRate, duration)
-		if *debug{
+		if *debug {
 			fmt.Printf("Max: %d, Min: %d, Mid: %d, success: %f\n", maxRate, minRate, midRate, metrics.Success)
 		}
 
 		if metrics.Success == 1.0 {
 			minRate = midRate
-		}else{
+		} else {
 			maxRate = midRate
 		}
 		time.Sleep(delay)
@@ -51,17 +50,17 @@ func binarrySearch(attacker vegeta.Attacker, targeter vegeta.Targeter, maxRate i
 }
 
 var (
-	duration = flag.Duration("duration", 10* time.Second, "Benchmark duration")
-	delay = flag.Duration("delay", 10 * time.Second, "Benchmark interval")
-	maxRate = flag.Int("rate", 10000, "Max Benchmark Rate(rps)")
+	duration = flag.Duration("duration", 10*time.Second, "Benchmark duration")
+	delay    = flag.Duration("delay", 10*time.Second, "Benchmark interval")
+	maxRate  = flag.Int("rate", 10000, "Max Benchmark Rate(rps)")
 	insecure = flag.Bool("insecure", false, "Ignore TLS errors")
-	debug = flag.Bool("debug", false, "Output Debug Logs")
-	output = flag.String("output", "text", "Output format [text, json]")
+	debug    = flag.Bool("debug", false, "Output Debug Logs")
+	output   = flag.String("output", "text", "Output format [text, json]")
 )
 
-func formatText(metrics vegeta.Metrics){
+func formatText(metrics vegeta.Metrics) {
 	fmt.Printf("Requests [total, rate, throughput]: %d, %f, %f\n", metrics.Requests, metrics.Rate, metrics.Throughput)
-	fmt.Printf("Durationt [total, attack, wait]: %.2fs, %.2fs, %.2fs\n", (metrics.Duration+metrics.Wait).Seconds(),metrics.Duration.Seconds(),metrics.Wait.Seconds())
+	fmt.Printf("Durationt [total, attack, wait]: %.2fs, %.2fs, %.2fs\n", (metrics.Duration + metrics.Wait).Seconds(), metrics.Duration.Seconds(), metrics.Wait.Seconds())
 	fmt.Printf("Success [ratio] %.2f%%\n", metrics.Success*100)
 }
 
@@ -70,7 +69,7 @@ func formatJson(metrics vegeta.Metrics) {
 	fmt.Println(string(bytes))
 }
 
-func main(){
+func main() {
 	flag.Parse()
 	if len(flag.Args()) != 1 {
 		fmt.Println("Need host for target")
@@ -82,20 +81,18 @@ func main(){
 		panic(err)
 	}
 
-
 	targeter := vegeta.NewStaticTargeter(vegeta.Target{
 		Method: "GET",
-		URL: host,
+		URL:    host,
 	})
 
 	tlsc := tls.Config{InsecureSkipVerify: *insecure}
 	attacker := vegeta.NewAttacker(vegeta.TLSConfig(&tlsc))
 
-
 	metrics := binarrySearch(*attacker, targeter, *maxRate, *duration, *delay)
 	if *output == "json" {
 		formatJson(metrics)
-	}else{
+	} else {
 		formatText(metrics)
 	}
 }
